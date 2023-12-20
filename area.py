f606b7d6e541f49a43c68affc94098dd99e79575
JingoBongo <justfortoma1607>
2023-12-20 12:04:40 +0200

    added quadratic equation calculation with visualisation

--- area.py
+++ area.py
@@ -19,8 +19,8 @@ def area_below_x_axis(a, b, c):
 
 def plot_quadratic_with_shading(a, b, c):
     x = np.linspace(-10, 10, 400)
-
-    # Calculate y values using the quadratic equation
+    #a b c указаны снизу
+    # ПРИМЕР КВАДРАТНОГО УРАВНЕНИЯ
     y = a * x ** 2 + b * x + c
 
     plt.fill_between(x, y, where=(y < 0), color='gray', alpha=0.5, label='Area Below x-axis')
@@ -29,19 +29,18 @@ def plot_quadratic_with_shading(a, b, c):
 
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.title('Quadratic Equation with Shaded Area Below x-axis')
+    plt.title('Квадратное уравнение с заштрихованной областью под осью x')
 
-    # Add a legend
+
     plt.legend()
 
-    # Show the plot
+
     plt.grid(True)
     plt.axhline(0, color='black', linewidth=0.5)
     plt.axvline(0, color='black', linewidth=0.5)
     plt.show()
 
 
-# Example usage
 a, b, c = 1, -1, -72
 area, error = area_below_x_axis(a, b, c)
 plot_quadratic_with_shading(a, b, c)
